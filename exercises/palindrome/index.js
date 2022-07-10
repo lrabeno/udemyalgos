91824260a051c152b7e6a2518b62468ac03569d3
Louis J Rabeno <lrabeno@gmail.com>
2022-07-10 13:46:13 -0400

    completed part 2

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,35 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//   const reversed = str.split('').reverse().join('');
+//   if (reversed !== str) {
+//     return false;
+//   }
+//   console.log(reversed);
+//   return true;
+// }
+
+// USING REDUCE
+// function palindrome(str) {
+//   const reversed = str.split('').reduce((word, letter) => {
+//     return letter + word;
+//   }, '');
+//   if (reversed !== str) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// Iterating using .every()
+// str[str.length - i - 1] means you subtract the
+// index from the back at i. You subtract what index from
+// the back and then every compares them from the front.
+
+// function palindrome(str) {
+//   return str.split('').every((char, i) => {
+//     return char === str[str.length - i - 1];
+//   });
+// }
 
 module.exports = palindrome;
