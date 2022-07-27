6032f354be2558bd6ef7abc7124626eb01fc4912
Louis J Rabeno <lrabeno@gmail.com>
2022-07-26 20:02:42 -0400

    completed capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -21,13 +21,28 @@
 
 // USING FOR OF LOOP
 
-function capitalize(str) {
-  const words = [];
+// function capitalize(str) {
+//   const words = [];
 
-  for (word of str.split(' ')) {
-    words.push(word[0].toUpperCase() + word.slice(1));
+//   for (word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return words.join(' ');
+// }
+
+// USING TRADITIONAL FOR LOOP
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
   }
-  return words.join(' ');
+  return result;
 }
 
 module.exports = capitalize;
