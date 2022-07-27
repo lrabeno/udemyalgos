4ebb644d701982d8c98ce9642112d645504904dc
Louis J Rabeno <lrabeno@gmail.com>
2022-07-26 21:34:35 -0400

    first step solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,21 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// ITERATIVE SOLUTION
+
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
