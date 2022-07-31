2858efa5aeafab84a3d9aa8b336b258fcbc85103
Louis J Rabeno <lrabeno@gmail.com>
2022-07-31 14:28:39 -0400

    iterative pyramid solutuon complete

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,22 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// ITERATIVE SOLUTION
+
+// function pyramid(n) {
+//   let midpoint = Math.floor((2 * n - 1) / 2);
+//   //   console.log('midpoint', midpoint);
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
 
 module.exports = pyramid;
