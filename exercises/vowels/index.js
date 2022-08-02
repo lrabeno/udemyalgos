a0afcce7af5d42ac47636fa1806a7f4d863447f8
Louis J Rabeno <lrabeno@gmail.com>
2022-08-01 22:46:18 -0400

    vowels better solution complete

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,23 +7,55 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {
-  let count = 0;
-  let lower = str.toLowerCase();
-  for (let i = 0; i <= lower.length - 1; i++) {
-    if (lower[i] === 'a') {
-      count++;
-    } else if (lower[i] === 'e') {
-      count++;
-    } else if (lower[i] === 'i') {
-      count++;
-    } else if (lower[i] === 'o') {
-      count++;
-    } else if (lower[i] === 'u') {
-      count++;
-    }
-  }
-  return count;
-}
+// function vowels(str) {
+//   let count = 0;
+//   const regex = /[aeiou]/gi;
+//   let result = str.match(regex);
+//   if (result.length === null) {
+//     return 0;
+//   } else {
+//     return result.length;
+//   }
+//   if (str.match(regex)) {
+//     count++;
+//   }
+//   for (let i = 0; i <= str.length; i++) {
+//     if (str[i] === regex) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// function vowels(str) {
+//   let count = 0;
+//   let lower = str.toLowerCase();
+//   for (let i = 0; i <= lower.length - 1; i++) {
+//     if (lower[i] === 'a') {
+//       count++;
+//     } else if (lower[i] === 'e') {
+//       count++;
+//     } else if (lower[i] === 'i') {
+//       count++;
+//     } else if (lower[i] === 'o') {
+//       count++;
+//     } else if (lower[i] === 'u') {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// function vowels(str) {
+//   let count = 0;
+//   let checker = ['a', 'e', 'i', 'o', 'u'];
+//   for (let i = 0; i <= str.length - 1; i++) {
+//     if (checker.includes(str[i].toLowerCase())) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
 module.exports = vowels;
