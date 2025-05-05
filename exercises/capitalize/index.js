a114b7599aff0c6f55f20663551f3fb61fa2a7f0
Louis Rabeno <lrabeno@gmail.com>
2025-05-05 11:11:45 -0400

    deleted solutions

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,42 +7,4 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-//USING FOR EACH
-
-// function capitalize(str) {
-//   const words = [];
-//   let arr = str.split(' ');
-
-//   arr.forEach((element, i) => {
-//     words.push(element[0].toUpperCase() + element.slice(1));
-//   });
-//   return words.join(' ');
-// }
-
-// USING FOR OF LOOP
-
-// function capitalize(str) {
-//   const words = [];
-
-//   for (word of str.split(' ')) {
-//     words.push(word[0].toUpperCase() + word.slice(1));
-//   }
-//   return words.join(' ');
-// }
-
-// USING TRADITIONAL FOR LOOP
-
-function capitalize(str) {
-  let result = str[0].toUpperCase();
-
-  for (let i = 1; i < str.length; i++) {
-    if (str[i - 1] === ' ') {
-      result += str[i].toUpperCase();
-    } else {
-      result += str[i];
-    }
-  }
-  return result;
-}
-
 module.exports = capitalize;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,40 +7,4 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-// REGEX SOLUTION
-function vowels(str) {
-  const matches = str.match(/[aeiou]/gi);
-  return matches ? matches.length : 0;
-}
-
-// function vowels(str) {
-//   let count = 0;
-//   let lower = str.toLowerCase();
-//   for (let i = 0; i <= lower.length - 1; i++) {
-//     if (lower[i] === 'a') {
-//       count++;
-//     } else if (lower[i] === 'e') {
-//       count++;
-//     } else if (lower[i] === 'i') {
-//       count++;
-//     } else if (lower[i] === 'o') {
-//       count++;
-//     } else if (lower[i] === 'u') {
-//       count++;
-//     }
-//   }
-//   return count;
-// }
-
-// function vowels(str) {
-//   let count = 0;
-//   let checker = ['a', 'e', 'i', 'o', 'u'];
-//   for (let i = 0; i <= str.length - 1; i++) {
-//     if (checker.includes(str[i].toLowerCase())) {
-//       count++;
-//     }
-//   }
-//   return count;
-// }
-
 module.exports = vowels;
