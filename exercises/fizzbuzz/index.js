a53c7de9cc4e7e4fb13c880719848cf02036a222
Louis Rabeno <lrabeno@gmail.com>
2025-05-22 12:00:05 -0400

    fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,4 +12,19 @@
 //   4
 //   buzz
 
+function fizzBuzz(num) {
+  // make sure to start at 1 because you dont want 0 to be a part of the fizzbuzz
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
 module.exports = fizzBuzz;
